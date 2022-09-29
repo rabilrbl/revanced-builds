1d677299f44c02556535d99bbb7851ab558ff736
Mohammed Rabil <rabil@techie.com>
2022-09-30 00:59:15 +0530

    Fix: downloads dir not found

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# if downloads directory does not exist, create it
+if [ ! -d "downloads" ]; then
+    mkdir downloads
+fi
+
 req() {
     url=$1
     # curl request with User-Agent
@@ -8,8 +13,8 @@ req() {
 
 
 # Download latest revanced-cli
-wget $(req https://api.github.com/repos/revanced/revanced-cli/releases/latest | jq .assets[0].browser_download_url | tr -d '"') -O "../downloads/revanced-cli.jar"
+wget $(req https://api.github.com/repos/revanced/revanced-cli/releases/latest | jq .assets[0].browser_download_url | tr -d '"') -O "downloads/revanced-cli.jar"
 
 # Download latest revanced-patches
-wget $(req https://api.github.com/repos/revanced/revanced-patches/releases/latest | jq .assets[1].browser_download_url | tr -d '"') -O "../downloads/revanced-patches.jar"
+wget $(req https://api.github.com/repos/revanced/revanced-patches/releases/latest | jq .assets[1].browser_download_url | tr -d '"') -O "downloads/revanced-patches.jar"
 
