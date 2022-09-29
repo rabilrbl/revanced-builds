fa120a417086986f2606ae303b8af621bbf8cd7d
Mohammed Rabil <rabil@techie.com>
2022-09-30 01:39:20 +0530

    setup.sh: fix permission issue

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -11,11 +11,11 @@ sudo apt-get -qq -y --no-install-recommends install gnupg software-properties-co
     curl -sLO https://cdn.azul.com/zulu/bin/zulu-repo_${ZULU_REPO_VER}_all.deb && sudo dpkg -i zulu-repo_${ZULU_REPO_VER}_all.deb && \
     sudo apt-get -qq update && \
     sudo mkdir -p /usr/share/man/man1 && \
-    echo "Package: zulu17-*\nPin: version 17.0.4.1-*\nPin-Priority: 1001" > /etc/apt/preferences && \
+    sudo echo "Package: zulu17-*\nPin: version 17.0.4.1-*\nPin-Priority: 1001" > /etc/apt/preferences && \
     sudo apt-get -qq -y --no-install-recommends install zulu17-jdk=17.0.4.1-* && \
     sudo apt-get -qq -y purge gnupg software-properties-common curl && \
     sudo apt -y autoremove && \
-    rm -rf /var/lib/apt/lists/* zulu-repo_${ZULU_REPO_VER}_all.deb
+    sudo rm -rf /var/lib/apt/lists/* zulu-repo_${ZULU_REPO_VER}_all.deb
 
 # export JAVA_HOME
 export JAVA_HOME=/usr/lib/jvm/zulu17-ca-amd64
