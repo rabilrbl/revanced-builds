20a21be98e12200c34fef309c16abd39f2f9a1a7
Mohammed Rabil <rabil@techie.com>
2022-09-30 01:12:13 +0530

    Refactor: prebuild.sh
    - new function to download revanced releases

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -6,15 +6,18 @@ if [ ! -d "downloads" ]; then
 fi
 
 req() {
-    url=$1
+    url=$1 extra_args=$2
     # curl request with User-Agent
-    curl -A "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0" $url
+    curl -A "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0" $2 $url
 }
 
+fetch_revanced_release() {
+    repo_name=$1 asset_no=$2 file_name=$3
+    wget $(req https://api.github.com/repos/revanced/$repo_name/releases/latest | jq -r ".assets[$asset_no].browser_download_url" | tr -d '"') -O downloads/$file_name
+}
 
 # Download latest revanced-cli
-wget $(req https://api.github.com/repos/revanced/revanced-cli/releases/latest | jq .assets[0].browser_download_url | tr -d '"') -O "downloads/revanced-cli.jar"
+fetch_revanced_release revanced-cli 0 revanced-cli.zip
 
 # Download latest revanced-patches
-wget $(req https://api.github.com/repos/revanced/revanced-patches/releases/latest | jq .assets[1].browser_download_url | tr -d '"') -O "downloads/revanced-patches.jar"
-
+fetch_revanced_release revanced-patches 0 revanced-patches.zip
