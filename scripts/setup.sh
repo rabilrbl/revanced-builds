adccf719188b7373fa286bb82e0f62e939a1bc15
Mohammed Rabil <rabil@techie.com>
2022-09-30 01:16:24 +0530

    update zulu 17 setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,20 +4,23 @@
 sudo apt-get -q update
 sudo apt-get -yq install gnupg curl jq wget tree
 
-# add Azul's public key
-sudo apt-key adv --keyserver "hkp://keyserver.ubuntu.com:80" --recv-keys "0xB1998361219BD9C9"
+# Azul Zulu OpenJDK setup
+apt-get -qq -y --no-install-recommends install gnupg software-properties-common locales curl tzdata && \
+    echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen && \
+    locale-gen en_US.UTF-8 && \
+    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xB1998361219BD9C9 && \
+    curl -sLO https://cdn.azul.com/zulu/bin/zulu-repo_${ZULU_REPO_VER}_all.deb && dpkg -i zulu-repo_${ZULU_REPO_VER}_all.deb && \
+    apt-get -qq update && \
+    apt-get -qq -y dist-upgrade && \
+    mkdir -p /usr/share/man/man1 && \
+    echo "Package: zulu17-*\nPin: version 17.0.4.1-*\nPin-Priority: 1001" > /etc/apt/preferences && \
+    apt-get -qq -y --no-install-recommends install zulu17-jdk=17.0.4.1-* && \
+    apt-get -qq -y purge gnupg software-properties-common curl && \
+    apt -y autoremove && \
+    rm -rf /var/lib/apt/lists/* zulu-repo_${ZULU_REPO_VER}_all.deb
 
-# download and install the package that adds 
-# the Azul APT repository to the list of sources 
-curl -O https://cdn.azul.com/zulu/bin/zulu-repo_1.0.0-3_all.deb
-
-# install the package
-sudo apt-get install ./zulu-repo_1.0.0-3_all.deb
-
-# update the package sources
-sudo apt-get update
-
-sudo apt-get install zulu17-jdk
+# export JAVA_HOME
+export JAVA_HOME=/usr/lib/jvm/zulu17-ca-amd64
 
 # should print zulu jdk version
 java -version
