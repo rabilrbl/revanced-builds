1ce70ffde7e5c104f5c94ff9e95c4b1c1b4d951a
Mohammed Rabil <rabil@techie.com>
2022-09-30 01:20:08 +0530

    Refactor zulu installations

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,18 +6,17 @@ sudo apt-get -yq install gnupg curl jq wget tree
 
 # Azul Zulu OpenJDK setup
 export ZULU_REPO_VER=1.0.0-3
-apt-get -qq -y --no-install-recommends install gnupg software-properties-common locales curl tzdata && \
+sudo apt-get -qq -y --no-install-recommends install gnupg software-properties-common locales curl tzdata && \
     echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen && \
     locale-gen en_US.UTF-8 && \
-    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xB1998361219BD9C9 && \
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xB1998361219BD9C9 && \
     curl -sLO https://cdn.azul.com/zulu/bin/zulu-repo_${ZULU_REPO_VER}_all.deb && dpkg -i zulu-repo_${ZULU_REPO_VER}_all.deb && \
-    apt-get -qq update && \
-    apt-get -qq -y dist-upgrade && \
-    mkdir -p /usr/share/man/man1 && \
+    sudo apt-get -qq update && \
+    sudo mkdir -p /usr/share/man/man1 && \
     echo "Package: zulu17-*\nPin: version 17.0.4.1-*\nPin-Priority: 1001" > /etc/apt/preferences && \
-    apt-get -qq -y --no-install-recommends install zulu17-jdk=17.0.4.1-* && \
-    apt-get -qq -y purge gnupg software-properties-common curl && \
-    apt -y autoremove && \
+    sudo apt-get -qq -y --no-install-recommends install zulu17-jdk=17.0.4.1-* && \
+    sudo apt-get -qq -y purge gnupg software-properties-common curl && \
+    sudo apt -y autoremove && \
     rm -rf /var/lib/apt/lists/* zulu-repo_${ZULU_REPO_VER}_all.deb
 
 # export JAVA_HOME
